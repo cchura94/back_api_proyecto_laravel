8bb425e49eab00c51dc5a797e72d0f1ccc9e5030
cchura94 <cchura.cpc@gmail.com>
2023-02-23 19:50:19 -0400

    Laravel Api Authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        # validar
+        $credenciales = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);      
+
+        // verificar correo y password
+        if(!Auth::attempt($credenciales)){
+            return response()->json(["message" => "No Autorizado"], 401);
+        }
+        
+        // generar token
+        $user = Auth::user();
+        $tokenResult = $user->createToken("Token Auth");
+        $token = $tokenResult->plainTextToken;
+        // respuesta
+
+        return response()->json([
+            "access_token" => $token,
+            "token_type" => "Bearer",
+            "usuario" => $user
+        ]);
+    }
+
+    public function registro(Request $request)
+    {
+        // validacion
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required",
+            "c_password" => "required|same:password"
+        ]);
+        // registrar usuario
+        $u = new User;
+        $u->name = $request->name;
+        $u->email = $request->email;
+        $u->password = bcrypt($request->password);
+        $u->save();
+
+        // respuesta
+        return response()->json(["message" => "El usuario ha sido registrado"], 201);
+    }
+
+    public function miPerfil()
+    {
+        $user = Auth::user();
+
+        return response()->json($user, 200);
+    }
+
+    public function cerrar(Request $request)
+    {
+        // Auth::user()->tokens()->delete();
+        $request->user()->tokens()->delete();
+
+        return response()->json(["message" => "Logout"], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//  /v1/auth
+Route::prefix('v1/auth')->group(function(){
+
+    // /v1/auth/login,   /v1/auth/register
+    Route::post("/login", [AuthController::class, "login"]);
+    Route::post("/register", [AuthController::class, "registro"]);
+
+    // /v1/auth/perfil,   /v1/auth/logout
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::get("/perfil", [AuthController::class, "miPerfil"]);
+        Route::post("/logout", [AuthController::class, "cerrar"]);
+    });
+});
