cef279787b3aed10ef60f5f5edc052804db8c250
cchura94 <cchura.cpc@gmail.com>
2023-03-08 20:27:44 -0400

    Producto lista y guardar

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
@@ -11,9 +12,23 @@ class ProductoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // /api/producto?page=2&limit=10&q=laptop&orderby=id
+        // $page = $request->page;
+        $limit = $request->limit?$request->limit:10;
+        $q = $request->q;
+        $orderby = $request->orderby?$request->orderby:'id';
+
+        if($q){
+            $productos = Producto::where('nombre', 'like', '%'.$q.'%')
+                                     ->orderBy($orderby, 'asc')
+                                     ->paginate($limit);
+        }else{
+            $productos = Producto::orderBy($orderby, 'desc')->paginate($limit);
+        }
+
+        return response()->json($productos, 200);
     }
 
     /**
@@ -24,7 +39,26 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validar
+        $request->validate([
+            "nombre" => 'required|min:3|max:200',
+            "categoria_id"=> "required"
+        ]);
+        // subir img
+        $direccion_imagen = "";
+
+        // guardar
+        $prod = new Producto();
+        $prod->nombre = $request->nombre;
+        $prod->precio = $request->precio;
+        $prod->stock = $request->stock;
+        $prod->descripcion = $request->descripcion;
+        $prod->categoria_id = $request->categoria_id;
+        $prod->nombre = $direccion_imagen;
+        $prod->save();
+
+        // responder
+        return response()->json(["mensaje" => "Producto registrado", "data" => $prod]);
     }
 
     /**
